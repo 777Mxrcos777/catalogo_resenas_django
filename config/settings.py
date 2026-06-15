62a530bf6f34b63d726422dfcc10a584b6fffcaf
Marcos Merchan <davidborre43@gmail.com>
2026-06-15 13:56:54 -0500

    Frontend completado con estilos CSS funcionando

--- config/settings.py
+++ config/settings.py
@@ -60,10 +60,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from apps.productos import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('productos/', views.lista_productos, name='lista_productos'),
-    path('producto/<int:pk>/', views.detalle_producto, name='detalle_producto'),
+    path('productos/<int:pk>/', views.detalle_producto, name='detalle_producto'),
     path('ranking/', views.ranking, name='ranking'),
     path('admin/', admin.site.urls),
 ]
+
+# Servir archivos estáticos en modo desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
